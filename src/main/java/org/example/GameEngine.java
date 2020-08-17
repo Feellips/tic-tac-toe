a98642119542670ebe626f156889d95a7281f749
NAGIBATOR_228 <filf@kt-segment.ru>
2020-08-17 16:41:08 +0300

    -method drawField was realized.

--- src/main/java/org/example/GameEngine.java
+++ src/main/java/org/example/GameEngine.java
@@ -5,12 +5,16 @@ package org.example;
  */
 public class GameEngine {
     public static void main(String[] args) {
-
-
+        Field field = new Field(3, 3);
+        drawField(field);
     }
 
-    public void drawField(Field field) {
+    public static void drawField(Field field) {
+        char[][] table = field.getTable();
 
+        for (int i = 0; i < field.getX(); i++)
+            for (int j = 0; j < field.getY(); j++)
+                System.out.println(table[i][j]);
     }
 }
 
